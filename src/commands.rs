c876d8935c3a122dd5a2e22efac05ce1abb94594
Bernardo F Pereira <bernardofpereira@live.com>
2024-06-15 17:12:56 +0000

    testing type not builtin

--- src/commands.rs
+++ src/commands.rs
@@ -28,13 +28,19 @@ pub fn command_type(cmd_args: String) {
         println!("{} is a shell builtin", cmd_args);
         return;
     } else {
-        // Look for executables in PATH
         let env_var = var_os("PATH");
         if let Some(var) = env_var {
             let path_dirs = split_paths(&var);
 
             for dir in path_dirs {
-                println!("{}", dir.display());
+                // println!("{}", dir.display());
+
+                for entry in dir.read_dir().unwrap() {
+                    let file_path = entry.unwrap().path();
+                    if file_path.display().to_string().contains(cmd_args) {
+                        println!("{} is {}", cmd_args, file_path.display())
+                    }
+                }
             }
         }
     }
