b669b57176ad21528c6d15e657ca0a62aae3cb9e
Bernardo F Pereira <bernardofpereira@live.com>
2024-06-16 14:19:25 +0000

    testing type not builtin

--- src/commands.rs
+++ src/commands.rs
@@ -40,12 +40,12 @@ pub fn command_type(cmd_args: String) {
                     // Finds anything that contains the chars in cmd_args
                     if file_path.display().to_string().ends_with(target.as_str()) {
                         println!("{} is {}", cmd_args, file_path.display());
-                    } else {
-                        println!("{}: not found", cmd_args);
                         return;
                     }
                 }
             }
+
+            println!("{}: not found", cmd_args);
         }
     }
 }
