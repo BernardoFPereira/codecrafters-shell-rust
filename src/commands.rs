0077627bdd10fefb21d4ed486d24d2e60033b0da
Beetledjuice <bernardofpereira@live.com>
2024-06-17 18:37:33 -0300

    Refactoring; not working yet

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,7 @@
 use crate::Command;
 use std::env::*;
 use std::fs::*;
+use std::path::*;
 use std::process::exit;
 
 pub fn command_echo(args: String) {
@@ -32,20 +33,39 @@ pub fn command_type(cmd_args: String) {
         if let Some(var) = env_var {
             let path_dirs = split_paths(&var);
 
-            for dir in path_dirs {
-                for entry in dir.read_dir().unwrap() {
-                    let mut target = String::from("/");
-                    target.push_str(cmd_args);
-                    let file_path = entry.unwrap().path();
-                    // Finds anything that contains the chars in cmd_args
-                    if file_path.display().to_string().ends_with(target.as_str()) {
-                        println!("{} is {}", cmd_args, file_path.display());
-                        return;
+            let mut entries = path_dirs
+                .flat_map(|dir| dir.read_dir().unwrap())
+                .filter(|entry| {
+                    let target = format!("{}{}", MAIN_SEPARATOR_STR, cmd_args);
+                    if let Ok(entry) = entry.as_ref() {
+                        entry.path().display().to_string().ends_with(&target)
+                    } else {
+                        false
                     }
-                }
+                });
+
+            if let Some(entry) = entries.next() {
+                println!("{} is {}", cmd_args, entry.display());
+            } else {
+                println!("{}: not found", cmd_args);
             }
 
-            println!("{}: not found", cmd_args);
+            println!("{:?}", entries.next());
+
+            // for dir in path_dirs {
+            //     for entry in dir.read_dir().unwrap() {
+            //         let mut target = String::from("/");
+            //         target.push_str(cmd_args);
+            //         let file_path = entry.unwrap().path();
+            //         // Finds anything that contains the chars in cmd_args
+            //         if file_path.display().to_string().ends_with(target.as_str()) {
+            //             println!("{} is {}", cmd_args, file_path.display());
+            //             return;
+            //         }
+            //     }
+            // }
+
+            // println!("{}: not found", cmd_args);
         }
     }
 }
